3fe7c7e118be257d954bdd5c44d81c498af058ef
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-13 21:13:52 +0200

    feat: added content - CTA Section - AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,10 +7,33 @@ export default {
 
 <template>
 
-<h2>footer</h2>
+   <!-- Call To Action - Section -->
+   <section>
+
+      <h3>We Create New Worlds!</h3>
+
+      <h2>Want Tips On Your <br> Storytelling?</h2>
+
+      <p>Join our newsletter for tips on how to elevate your brand through storytelling</p>
+
+      <div>
+
+         <input type="email" aria-label="Enter your email">
+
+         <button>Submit</button>
+
+      </div>
+
+
+   </section>
+   <!-- /Call To Action - Section -->
+
+   <!-- Footer -->
+   <footer>
+
+   </footer>
+   <!-- /Footer -->
 
 </template>
 
-<style lang="scss" scoped>  
-
-</style>
+<style lang="scss" scoped></style>

8717d458b33a108ec5840afa671af55a3173fe50
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-13 22:33:09 +0200

    feat: added content - Footer Section - AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,51 @@
 <script>
+
 export default {
+   data() {
+      return {
+
+         // General Menu Links
+         menu: [
+            {
+               title: "About",
+               routeName: "about",
+            },
+            {
+               title: "Portfolio",
+               routeName: "portfolio",
+            },
+            {
+               title: "Pricing",
+               routeName: "pricing",
+            },
+            {
+               title: "Career",
+               routeName: "career",
+            },
+            {
+               title: "Contact",
+               routeName: "contact",
+            },
+         ],
+
+         // Social Networks list
+         socialsMenu: [
+            {
+               name: "facebook",
+               iconClasses: "fa-brands fa-facebook-f",
+            },
+            {
+               name: "twitter",
+               iconClasses: "fa-brands fa-twitter",
+            },
+            {
+               name: "instagram",
+               iconClasses: "fa-brands fa-instagram",
+            },
+
+         ],
+      }
+   }
 
 }
 
@@ -30,6 +76,57 @@ export default {
 
    <!-- Footer -->
    <footer>
+
+      <!-- Footer Menu - Section -->
+      <section>
+
+         <!-- Footer Logo -->
+         <img src="../assets/Img/main-img/footer-logo.png" alt="Logo Anidio">
+         <!-- /Footer Logo -->
+
+         <!-- Footer Menu -->
+         <ul>
+
+            <li v-for="item in menu">
+
+               <router-link>
+
+                  {{ item.title }}
+
+               </router-link>
+
+            </li>
+
+         </ul>
+         <!-- /Footer Menu -->
+
+      </section>
+      <!-- /Footer Menu - Section -->
+
+      <!-- Footer Copyright - Section -->
+      <section>
+
+         <p>Copyright © 2022 <a href="https://www.templatemonster.com/authors/softivus/">Softivus</a>. All rights
+            reserved. </p>
+
+         <!-- Footer Menu -->
+         <ul>
+
+            <li v-for="social in socialsMenu">
+
+               <router-link>
+
+                  <i :class="`${social.iconClasses}`"></i>
+                  
+               </router-link>
+
+            </li>
+
+         </ul>
+         <!-- /Footer Menu -->
+
+      </section>
+      <!-- /Footer Copyright - Section -->
 
    </footer>
    <!-- /Footer -->

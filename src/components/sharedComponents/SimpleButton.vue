8575d4dad0cf3b3ca3e11e376b845dafeea9fcf3
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-14 13:43:03 +0200

    feat: added hover behaviour - SimpleButton

--- src/components/sharedComponents/SimpleButton.vue
+++ src/components/sharedComponents/SimpleButton.vue
@@ -5,7 +5,7 @@ export default {
 
       bgColor: String,
       color: String,
-      
+
    }
 
 }
@@ -19,14 +19,20 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-
 .ms-btn {
    padding: 12px 38px 12px;
    display: inline-block;
    box-shadow: 0px 8px 18px 0px rgba(109, 100, 139, 0.61);
    border: 1px solid rgba(0, 0, 0, 0);
    transition: 0.7s;
-   
-}
 
+   &:hover {
+      transition: 0.7s;
+      color: white;
+      border: 1px solid white;
+      background-color: rgba(0, 0, 0, 0);
+
+   }
+
+}
 </style>

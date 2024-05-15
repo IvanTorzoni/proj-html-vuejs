73f94f672f250b3dcabcf8bb47fc8d6afffa9308
lucasens1 <lucasensini1@gmail.com>
2024-05-15 13:09:32 +0200

    add : background shapes with animations

--- src/components/sharedComponents/AppTeam.vue
+++ src/components/sharedComponents/AppTeam.vue
@@ -37,13 +37,23 @@ export default {
 </script>
 
 <template>
-   <div class="container pt-5 pb-5">
+   <!-- Container Membri-->
+   <div class="container team-cont pt-5 pb-5">
+      <div class="shape-1">
+         <img src="../../assets/img/about-img/shape.png" alt="">
+      </div>
+      <div class="shape-2">
+         <img src="../../assets/img/about-img/shape2.png" alt="">
+      </div>
+      <!--Sezione titoli-->
       <div class="text-center">
          <h5 class="ms-text-l-purple pb-2"> Anidio Magic Team Member </h5>
          <h1 class="pb-2">Let Us Animate Your Project </h1>
          <p class="pb-5">We create new worlds! Let's collaborate and create engaging, effective, award-winning animations </p>
       </div>
+      <!-- /Titoli-->
 
+      <!--Slider Membri del team -->
       <div class="team-slider">
          <div class="team-slide-track d-flex gap-4 justify-content-between align-items-center h-100 p-5" :style="{ width: trackWidth }">
             <div class="member-slide col-4 text-center p-2" v-for="item in teamInfo">
@@ -58,14 +68,43 @@ export default {
             </div>
          </div>
       </div>
+      <!-- /Slider Membri del team-->
    </div>
-   
-
+   <!--/Container Membri-->
 </template>
 
 <style lang="scss">
+.team-cont{
+   position: relative;
+}
+/* Background Shapes */
+.shape-1{
+   position: absolute;
+   top : 0;
+   left: 0;
+   z-index: -1;
+   animation: bounce 3s infinite alternate;
+   max-width: 580px;
+}
+.shape-2{
+   position: absolute;
+   top: 0;
+   right: 0;
+   z-index: -1;
+   animation: bounce 3s infinite alternate;
+   max-width: 580px;
+}
+/* Rimbalzo */
+@keyframes bounce {
+   0%, 100% {
+      transform: translateX(0);
+   }
+   50% {
+      transform: translateX(-30px);
+   }
+}
+/* Team - Slider */
 .team-slider {
-   background: white;
    margin: auto;
    overflow: hidden;
    position: relative;

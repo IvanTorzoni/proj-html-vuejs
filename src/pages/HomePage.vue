df651a36630664bc66eb25dc7c8f6dcdf221e8d1
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-15 12:16:08 +0200

    feat: added graphic part to our process section (logic is WIP)

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -25,6 +25,7 @@ export default {
 
     <AppFacts />
 
+    <!-- Work Section -->
     <section class="work-section pt-120">
         <div class="container">
             <div class="container">
@@ -46,7 +47,7 @@ export default {
                     <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.2s"
                         style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
                         <div class="work-thumb">
-                            <img src="../assets/Img/main-img//work1.png" alt="work-img">
+                            <img src="../assets/Img/main-img/work1.png" alt="work-img">
                             <div class="video-overlay">
                                 <a href="https://www.youtube.com/watch?v=sNub3X8fHew" class="video-btn">
                                     <i class="fas fa-play"></i>
@@ -57,7 +58,7 @@ export default {
                     <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.4s"
                         style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
                         <div class="work-thumb">
-                            <img src="../assets/Img/main-img//work2.png" alt="work-img">
+                            <img src="../assets/Img/main-img/work2.png" alt="work-img">
                             <div class="video-overlay">
                                 <a href="https://www.youtube.com/watch?v=sNub3X8fHew" class="video-btn">
                                     <i class="fas fa-play"></i>
@@ -68,7 +69,7 @@ export default {
                     <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.6s"
                         style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">
                         <div class="work-thumb">
-                            <img src="../assets/Img/main-img//work3.png" alt="work-img">
+                            <img src="../assets/Img/main-img/work3.png" alt="work-img">
                             <div class="video-overlay">
                                 <a href="https://www.youtube.com/watch?v=sNub3X8fHew" class="video-btn">
                                     <i class="fas fa-play"></i>
@@ -79,7 +80,7 @@ export default {
                     <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.8s"
                         style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInUp;">
                         <div class="work-thumb">
-                            <img src="../assets/Img/main-img//work4.png" alt="work-img">
+                            <img src="../assets/Img/main-img/work4.png" alt="work-img">
                             <div class="video-overlay">
                                 <a href="https://www.youtube.com/watch?v=sNub3X8fHew" class="video-btn">
                                     <i class="fas fa-play"></i>
@@ -99,7 +100,90 @@ export default {
         </div>
     </section>
 
-    <h2>process</h2>
+    <!-- Process Section -->
+    <section class="process-section pt-120 pb-120">
+        <div class="container">
+            <div class="row align-items-center justify-content-between flex-row-reverse">
+                <div class="col-xl-6">
+                    <div class="process-content">
+                        <div class="section-header wow fadeInDown" data-wow-delay="0.3s"
+                            style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInDown;">
+                            <h5 class="section-cete">
+                                Our Process
+                            </h5>
+                            <h2 class="section-title">
+                                Our Process for Your
+                                Animation Production
+                            </h2>
+                            <p>
+                                We have an effective process for working on animation
+                            </p>
+                        </div>
+                        <div class="process-slider owl-theme owl-carousel owl-loaded owl-drag">
+
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage">
+                                    <div class="owl-item">
+                                        <div class="process-items">
+                                            <div class="thumb">
+                                                <img src="../assets/Img/main-img/process1.png" alt="process-img">
+                                            </div>
+                                            <div class="process-content">
+                                                <h4>
+                                                    <a href="#0">Scripting</a>
+                                                </h4>
+                                                <p>
+                                                    We’ll take your idea and create a technical script which consists of
+                                                    action notes and animation descriptions
+                                                </p>
+                                            </div>
+                                            <span class="badge">
+                                                1
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item">
+                                        <div class="process-items">
+                                            <div class="thumb">
+                                                <img src="../assets/Img/main-img/process2.png" alt="process-img">
+                                            </div>
+                                            <div class="process-content">
+                                                <h4>
+                                                    <a href="#0">Pre- Production</a>
+                                                </h4>
+                                                <p>
+                                                    We’ll take your idea and create a technical script which consists of
+                                                    action notes and animation descriptions
+                                                </p>
+                                            </div>
+                                            <span class="badge">
+                                                2
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav">
+                                <button type="button" role="presentation" class="owl-prev"><i
+                                        class="fas fa-arrow-left"></i></button>
+                                <button type="button" role="presentation" class="owl-next"><i
+                                        class="fas fa-arrow-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5">
+                    <div class="process-thumb">
+                        <img src="../assets/Img/main-img/process-thumb.png" alt="process-img">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rocket">
+            <img src="../assets/Img/main-img/smart4.png" alt="rocket">
+        </div>
+    </section>
+    <!-- Process Section -->
 
     <AppTeam />
 
@@ -109,7 +193,6 @@ export default {
 
 <style scoped lang="scss">
 //WORK SECTION
-
 .work-section {
     padding-top: 120px;
 
@@ -151,7 +234,7 @@ export default {
         transition: all 0.3s;
 
         .video-overlay {
-            
+
             .video-btn {
                 width: 60px;
                 height: 60px;
@@ -176,7 +259,7 @@ export default {
     .work-btn {
         text-align: center;
         margin: 45px 0 0 auto;
-        
+
         .cmn--border {
             box-shadow: none;
             color: black;
@@ -189,7 +272,7 @@ export default {
             box-shadow: 0px 8px 18px 0px rgba(109, 100, 139, 0.61);
         }
 
-        .cmn--border:hover{
+        .cmn--border:hover {
             box-shadow: none;
             color: white;
             border-radius: 100px;
@@ -206,4 +289,188 @@ export default {
 
 }
 
-//END OF WORK SECTION</style>
+//END OF WORK SECTION
+
+//PROCESS SECTION
+.process-section {
+    flex-wrap: wrap;
+    position: relative;
+    overflow: hidden;
+    padding: 120px 0;
+
+    .section-header {
+        position: relative;
+        margin-bottom: 50px;
+
+        .section-cete {
+            margin-bottom: 25px;
+            color: #4d36dc;
+        }
+
+        .section-title {
+            font-weight: 700;
+            color: black;
+            margin-bottom: 5px;
+            font-size: 57px;
+            line-height: 74.1px;
+            margin-top: -12px;
+            font-family: "Chakra Petch", sans-serif;
+        }
+
+        p {
+            font-size: 18px;
+            max-width: 600px;
+            color: black;
+        }
+    }
+
+    .owl-stage-outer {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+
+        .owl-stage {
+            display: flex;
+        }
+        
+        .process-items {
+            border-radius: 20px;
+            padding: 20px 20px;
+            border: 1px solid lightgrey;
+            transition: 0.8s;
+            text-align: center;
+            position: relative;
+            width: 95%;
+    
+            .thumb {
+                width: 140px;
+                height: 140px;
+                line-height: 140px;
+                border-radius: 50%;
+                margin: 0 auto;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                transition: all 2s;
+            }
+    
+            .badge {
+                background: #f7f0ff;
+                color: #8d829c;
+                width: 44px;
+                height: 82px;
+                display: block;
+                font-size: 18px;
+                border-radius: 0;
+                display: flex;
+                align-items: flex-end;
+                justify-content: center;
+                padding: 15px 5px;
+                border-radius: 0 0 100px 100px;
+                position: absolute;
+                top: 0;
+                right: 20px;
+            }
+    
+            .process-content {
+                padding: 30px 0 0;
+    
+                h4 {
+                    font-weight: 600;
+                    margin-bottom: 5px;
+                    font-size: 32px;
+                    line-height: 40.2px;
+                    margin-top: -15px;
+                    font-family: "Chakra Petch", sans-serif;
+                }
+    
+                p {
+                    font-size: 18px;
+                    line-height: 30px;
+                    margin-bottom: 0;
+                    font-family: "Barlow", sans-serif;
+                }
+    
+            }
+        }
+    }
+
+
+
+    .owl-nav {
+        margin-top: 10px;
+
+        .owl-prev {
+            color: lightgrey;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            line-height: 50px;
+            border-radius: 50%;
+            transition: all 0.3s;
+            border: 1px solid lightgray;
+            margin-right: 10px;
+        }
+
+        .owl-prev:hover {
+            color: white;
+            background-color: orange;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            line-height: 50px;
+            border-radius: 50%;
+            transition: all 0.3s;
+            border: 1px solid orange;
+            margin-right: 10px;
+        }
+
+        .owl-next {
+            color: lightgrey;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            line-height: 50px;
+            border-radius: 50%;
+            transition: all 0.3s;
+            border: 1px solid lightgray;
+        }
+
+        .owl-next:hover {
+            color: white;
+            background-color: orange;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            line-height: 50px;
+            border-radius: 50%;
+            transition: all 0.3s;
+            border: 1px solid orange;
+            margin-right: 10px;
+        }
+    }
+
+
+    //Img a sinistra 
+    .process-thumb {
+        direction: rtl;
+
+        img {
+            max-width: 1000px;
+        }
+    }
+
+    // Razzo
+    .rocket {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
+
+//END OF PROCESS SECTION</style>

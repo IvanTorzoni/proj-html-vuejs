08920ba1a70d638262f00002a3feeb5e90fb3327
lucasens1 <lucasensini1@gmail.com>
2024-05-15 11:52:58 +0200

    minor fix

--- src/components/sharedComponents/AppClients.vue
+++ src/components/sharedComponents/AppClients.vue
@@ -14,7 +14,7 @@ export default {
      },
      // Calcola lunghezza, 
      trackWidth() {
-       return `${this.duplicatedSponsorImages.length * 225}px`;
+       return `${this.duplicatedSponsorImages.length * 250}px`;
      }
    },
    methods: {
@@ -62,5 +62,6 @@ export default {
  .slide {
    height: auto;
    width: 250px;
+   transition: 0.7s;
  }
  </style>
